43ec8769f8622805796e8edccebb8d75aa306959
Aashish Gupta <aashishg8160@gmail.com>
2026-06-04 23:48:39 +0530

    Fix environment lighting glare on reflective floor

--- src/components/canvas/CustomEnvironment.tsx
+++ src/components/canvas/CustomEnvironment.tsx
@@ -4,12 +4,15 @@ export default function CustomEnvironment({ intensity = 1 }: { intensity?: numbe
   return (
     <Environment resolution={256} environmentIntensity={intensity}>
       <group rotation={[-Math.PI / 4, -0.3, 0]}>
-        <Lightformer form="ring" intensity={5} scale={10} position={[-5, 5, -5]} target={[0, 0, 0]} />
+        {/* Main Key Light */}
+        <Lightformer form="ring" intensity={3} scale={10} position={[-5, 5, -5]} target={[0, 0, 0]} />
+        {/* Fill Light */}
         <Lightformer form="rect" intensity={2} scale={10} position={[5, 5, 5]} target={[0, 0, 0]} />
-        <Lightformer form="circle" intensity={3} scale={20} position={[0, -5, 0]} target={[0, 0, 0]} />
-        {/* Fill lights */}
-        <Lightformer form="rect" intensity={1} scale={10} position={[-10, 0, 5]} target={[0, 0, 0]} />
-        <Lightformer form="rect" intensity={1} scale={10} position={[10, 0, -5]} target={[0, 0, 0]} />
+        {/* Top/Overhead Soft Light */}
+        <Lightformer form="circle" intensity={1} scale={20} position={[0, 10, 0]} target={[0, 0, 0]} />
+        {/* Rim Lights */}
+        <Lightformer form="rect" intensity={0.5} scale={10} position={[-10, 5, 5]} target={[0, 0, 0]} />
+        <Lightformer form="rect" intensity={0.5} scale={10} position={[10, 5, -5]} target={[0, 0, 0]} />
       </group>
     </Environment>
   );
